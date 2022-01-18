a96f4afe8781758014cb6179c0b5c5fbca23e8fc
Nilesh Aggarwal <agnilesh8199@gmail.com>
2022-01-18 03:22:55 -0500

    Fix: Aligning text to center

--- src/partials/About.js
+++ src/partials/About.js
@@ -4,7 +4,7 @@ function About({ title, description }) {
   return (
     <div className="max-w-4xl mx-auto mt-16">
       <p className="text-2xl hover:text-gray-700 md:text-4xl font-bold text-center">{title}</p>
-      <p className="text-base text-left md:text-center whitespace-pre-line text-gray-600 leading-relaxed mt-4">
+      <p className="text-base text-center md:text-center whitespace-pre-line text-gray-600 leading-relaxed mt-4">
         {description}
       </p>
     </div>
