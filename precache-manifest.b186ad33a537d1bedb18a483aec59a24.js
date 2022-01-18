84648a7679d2e8657e50ac6316966488cac7a334
Nilesh Aggarwal <agnilesh8199@gmail.com>
2022-01-18 03:08:01 -0500

    Updates

---
+++ precache-manifest.b186ad33a537d1bedb18a483aec59a24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fb3c412b1a7ddfb245536970f7c8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d907550461d148f2802",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "31446b8f7dc3170ff7bb",
+    "url": "/static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "5d907550461d148f2802",
+    "url": "/static/js/2.c5f39a45.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "/static/js/2.c5f39a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31446b8f7dc3170ff7bb",
+    "url": "/static/js/main.32a93b09.chunk.js"
+  },
+  {
+    "revision": "c044c296fef9e582399f",
+    "url": "/static/js/runtime-main.4070baf2.js"
+  },
+  {
+    "revision": "15ec18fa48cfce76cf76a0f1e430886e",
+    "url": "/static/media/profile-img.15ec18fa.webp"
+  }
+]);
